5a1437ff77fc624ba64f36d551c4f961dda3e2dd
lovelotus <opera.prem@gmail.com>
2022-07-18 22:35:13 +0530

    Added Start Game At Position 0 UC_1

--- SnackAndLadder/SnackAndLadder/Program.cs
+++ SnackAndLadder/SnackAndLadder/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using SnakeAndLadderProgram;
+
 Console.WriteLine("Welcome to Snake And Ladder Game");
+SnakeAndLadder snakeAndLadder = new SnakeAndLadder();
+snakeAndLadder.StartGame();
+Console.ReadLine();
