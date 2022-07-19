39db4fb1b9a5640890349980ba0d118c6739d4b7
lovelotus <opera.prem@gmail.com>
2022-07-19 22:50:12 +0530

    Added Number of dice Rolled to Win And Player Position

--- SnackAndLadder/SnackAndLadder/SnakeAndLadder.cs
+++ SnackAndLadder/SnackAndLadder/SnakeAndLadder.cs
@@ -14,15 +14,17 @@ namespace SnakeAndLadderProgram
         private int PlayerPosition;
         public const int Winning = 100;
         int StartPoint = 0;
+        int Count = 0;
         public void StartGame()
 
         {
             Console.WriteLine("Game Started");
             Console.WriteLine("Player Position:", PlayerPosition);
             Random random = new Random();
-            
+
             while (StartPoint < Winning)//while loop loops through a block of code as long as a specified condition is True
             {
+                Count++;
                 int DiceRolled = random.Next(1, 7);
                 int Option = random.Next(0, 3);
                 switch (Option)//switch statement to select one of many code blocks to be executed.
@@ -40,7 +42,7 @@ namespace SnakeAndLadderProgram
                         }
                         Console.WriteLine("Player Position : " + StartPoint);
                         break;
-                        case snake:
+                    case snake:
                         StartPoint -= DiceRolled;
                         Console.WriteLine("Dice Rolls Value : -{0}", DiceRolled);
                         Console.WriteLine("Snake Attack : " + StartPoint);
@@ -51,12 +53,18 @@ namespace SnakeAndLadderProgram
                         Console.WriteLine("Player Position : " + StartPoint);
                         break;
                 }
-                        //if (StartPoint >= Winning)
-                        if (StartPoint == Winning)//if to specify a block of code to be executed, if a specified condition is true
-                        {
-                        Console.WriteLine("Won");
-                        }
+                //if (StartPoint >= Winning)
+                if (StartPoint == Winning)//if to specify a block of code to be executed, if a specified condition is true
+                {
+                   // Console.WriteLine("Won");
+                    Console.WriteLine(" Num of times dice rolled : {0}", +Count);
+                    Console.WriteLine("======PLAYER WON======");
+                    Console.WriteLine("*****GAME END*****");
+
+                }
             }
+
+            
 
         }
 
